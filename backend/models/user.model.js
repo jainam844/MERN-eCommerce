aa51e0c1b83f1bae7ec7068379d21be3a88c0e14
JAINAM SHAH <shahjainam491@gmail.com>
2025-04-03 18:14:27 +0530

    crud

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -41,7 +41,6 @@ const userSchema = new mongoose.Schema(
         timestamps: true,
     }
 );
-const User = mongoose.model.User("User", userSchema);
 
 userSchema.pre("save", async function (next) {
     if (!this.isModified("password")) return next();
@@ -56,6 +55,7 @@ userSchema.pre("save", async function (next) {
 
 userSchema.methods.comparePassword = async function (password) {
     return bcrypt.compare(password, this.password);
-  };
-  
+};
+const User = mongoose.model("User", userSchema);
+
 export default User;
