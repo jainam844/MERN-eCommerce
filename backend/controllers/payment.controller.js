4a1ec1d577849ce56dbec7fcd8be93556f6af876
JAINAM SHAH <shahjainam491@gmail.com>
2025-04-04 19:19:33 +0530

    STRIPE PAYMENT

---
+++ backend/controllers/payment.controller.js
@@ -0,0 +1,15 @@
+export const createCheckoutSession = async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+export const checkoutSuccess = async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+}
--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes)
 app.use("/api/cart", cartRoutes)
 app.use("/api/coupons", couponRoutes)
+app.use("/api/payment", paymentRoutes)
 
 
 app.listen(5000, () => {
