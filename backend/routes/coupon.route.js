14a63385b2738194b3b2089c487529f4d5bc69a0
JAINAM SHAH <shahjainam491@gmail.com>
2025-04-04 19:04:34 +0530

    COUPON COMPLETED

--- backend/routes/coupon.route.js
+++ backend/routes/coupon.route.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware";
+import { getCoupon, validateCoupon } from "../controllers/coupon.controller";
 
 const router = express.Router();
 
-router.post("/", protectRoute, getCoupon);
+router.get("/", protectRoute, getCoupon);
+router.get("/validate", protectRoute, validateCoupon);
 
 
 export default router;
