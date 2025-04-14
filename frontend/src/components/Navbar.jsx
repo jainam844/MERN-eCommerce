b6b95abb8a65ed365c7b1deb84b72827b15519a0
JAINAM SHAH <shahjainam491@gmail.com>
2025-04-14 20:54:44 +0530

    cart product

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,12 @@
 import { ShoppingCart, UserPlus, LogIn, LogOut, Lock } from "lucide-react";
 import { Link } from "react-router-dom";
 import { useUserStore } from "../stores/useUserStore";
+import { useCartStore } from "../stores/useCartStore";
 
 
 const Navbar = () => {
     const { user, logout } = useUserStore();
+    const { cart } = useCartStore();
     const isAdmin = user?.role === "admin";
     console.log(isAdmin)
     return (
@@ -31,14 +33,14 @@ const Navbar = () => {
                             >
                                 <ShoppingCart className='inline-block mr-1 group-hover:text-emerald-400' size={20} />
                                 <span className='hidden sm:inline'>Cart</span>
-                                {/* {cart.length > 0 && (
-									<span
-										className='absolute -top-2 -left-2 bg-emerald-500 text-white rounded-full px-2 py-0.5 
+                                {cart.length > 0 && (
+                                    <span
+                                        className='absolute -top-2 -left-2 bg-emerald-500 text-white rounded-full px-2 py-0.5 
 									text-xs group-hover:bg-emerald-400 transition duration-300 ease-in-out'
-									>
-										{cart.length}
-									</span>
-								)} */}
+                                    >
+                                        {cart.length}
+                                    </span>
+                                )}
                             </Link>
                         )}
                         {isAdmin && (
--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,13 +1,19 @@
-import toast from "react-hot-toast";
 import { ShoppingCart } from "lucide-react";
-// import { useUserStore } from "../stores/useUserStore";
-// import { useCartStore } from "../stores/useCartStore";
+import { useUserStore } from "../stores/useUserStore";
+import { useCartStore } from "../stores/useCartStore";
+import toast from "react-hot-toast";
 
 const ProductCard = ({ product }) => {
-    // const { user } = useUserStore();
-    // const { addToCart } = useCartStore();
+    const { user } = useUserStore();
+    const { addToCart } = useCartStore();
     const handleAddToCart = () => {
-
+        if (!user) {
+            toast.error("Please login to add products to cart", { id: "login" });
+            return;
+        }
+        else {
+            addToCart(product);
+        }
     }
 
     return (
